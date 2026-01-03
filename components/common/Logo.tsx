c7217bfd8c552a32172e56ab8cbf15878cf9082b
Ad7Store <ad7store.log@gmail.com>
2026-01-02 23:24:53 -0800

    Update Logo.tsx

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { ShoppingCart } from "lucide-react";
+import { AD7STORE } from "lucide-react";
 
 interface Props {
   className?: string;
